0c259bc1e3ff6d302e3d20f38231a1b129159b23
rohit-cyber <rohitjune15@gmail.com>
2021-04-23 16:59:25 +0530

    login and register api

---
+++ ems/myapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+GENDER_CHOICES=(('Male','Male'),
+                ('Female','Female'))
+
+
+class Profile(models.Model):
+    name=models.OneToOneField(User,on_delete=models.CASCADE)
+    city=models.CharField(max_length=250)
+    gender=models.CharField(choices=GENDER_CHOICES,max_length=250)
+    
+
+    def __str__(self):
+        return str(self.name)
+
+class 
